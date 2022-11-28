eddb4950969e638f58b3ff26e467cfaf3cb270ce
Jörg Daubert <kannx@kannx.net>
2022-11-28 14:11:49 +0100

    Übung 1

---
+++ Uebung1/app/src/main/java/com/dasgrau/uebung1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dasgrau.uebung1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.toString();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "in onCreate");
+    }
+}
